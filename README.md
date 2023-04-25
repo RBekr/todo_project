839d448e38bc6ca0d1ea6d5c5068e5c986c65231
RBekr <47711432+RBekr@users.noreply.github.com>
2023-04-25 15:59:24 +0700

    todo docker settings

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # todo_project
 TODO LIST
+
+## Setup
+```
+$ git clone https://github.com/RBekr/api_yamdb.git
+$ cd infra_sp2/infra
+$ docker-compose up --build -d
+$ docker-compose exec web python manage.py makemigrations
+$ docker-compose exec web python manage.py migrate
+$ docker-compose exec web python manage.py createsuperuser
+$ docker-compose exec web python manage.py collectstatic --no-input
+$ docker-compose exec web python manage.py loaddata fixtures.json
+```
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -16,7 +16,11 @@ SECRET_KEY = 'django-insecure-j)f2m@#s*-u(8b=%14+$zdxt%bo%n+7zzhez7prqf+@wh0vd23
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'web',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -70,11 +74,14 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', os.path.join(BASE_DIR, 'postgres')),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -111,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
